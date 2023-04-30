625ac8cc3e2fdca6f91ff85e0fb83e5b65bc6125
noriho137 <nori_ho@nifty.com>
2023-04-30 16:34:21 +0900

    Delete unnecessary codes.

--- config/settings.py
+++ config/settings.py
@@ -24,11 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-z0!uy%d5loerx$ej-@a2u5x@9epgp#$kuxxhc0sikcuv(%in#_'
 SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
 ALLOWED_HOSTS = []
